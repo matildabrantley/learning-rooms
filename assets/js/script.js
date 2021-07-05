af8bdaa5fab3806f068362a88d748c8fd5c0e1fc
Matilda Brantley <matildahbrantley@gmail.com>
2021-07-05 16:26:48 -0400

    islands changed to rooms, rooms form rows of 5, floor background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 var worldContainer = document.getElementById("world");
 var frameCount = 0; //for controlling framerate, e.g. skipping every other frame
 var fpsReduction = 1; //1 = normal fps, 2 = half fps, 60 = 1 fps, and so forth
-var islands = [];
-var numIslands = 1;
-var initialPop = 5; //initial island population
-var islandNames = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N',
+var rooms = [];
+var numIslands = 20;
+var initialPop = 5; //initial room population
+var roomNames = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N',
                       'O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var roomFloors = ['/assets/images/carpet1.jpg', '/assets/images/carpet2.jpg'];
+var roomSize = 100;
 var mouseX = 0, mouseY = 0;
-var learningPeriod = 1000; //number of frames that backpropagation occurs
+var learningPeriod = 200; //number of frames that backpropagation occurs
 
 //Create a Pixi Application
 let app = new PIXI.Application({antialias: true });
@@ -17,46 +19,43 @@ const graphics = new PIXI.Graphics();
 
 //Something for cats/creatures to chase
 class RedDot {
-    constructor(island, vector)
+    constructor(room, vector)
     {
-        this.island = island;
+        this.room = room;
         this.position = vector;
-        this.velocity = new Vector2d(0, 0, -20, 20);
+        this.velocity = new Vector2d(0, 0, -10, 10);
     }
 
     draw() {
-        graphics.lineStyle(2, 0x800000); 
+        graphics.lineStyle(2, 0xff0000); 
         graphics.beginFill(0xff0000);
-        graphics.drawCircle(this.position.x, this.position.y, 3);
+        graphics.drawCircle(this.position.x + this.room.minX, this.position.y + this.room.minY, 3);
         graphics.endFill();
     }
 
     update () {
+        var rebound = 10;
         //out of bounds check
-        if (this.position.x > this.island.maxX-20){
-            //this.velocity.reverse();
+        if (this.position.x > this.room.size-rebound){
             this.velocity.x = -this.velocity.x * 0.8;
             this.position.x -= 10;
         }
-        if (this.position.y > this.island.maxY-20){
-            //this.velocity.reverse();
+        if (this.position.y > this.room.size-rebound){
             this.velocity.y = -this.velocity.y * 0.8;
             this.position.y -= 10;
         }
-        if (this.position.x < this.island.minX+20){
-            //this.velocity.reverse();
+        if (this.position.x < rebound){
             this.velocity.x  = -this.velocity.x * 0.8;
             this.position.x += 10;
         }
-        if (this.position.y < this.island.minY+20){
-            //this.velocity.reverse();
+        if (this.position.y < rebound){
             this.velocity.y = -this.velocity.y * 0.8;
             this.position.y += 10;
         }
 
         // mouse position
-        //this.position.x = mouseX;
-        //this.position.y = mouseY;
+        // this.position.x = mouseX;
+        // this.position.y = mouseY;
         if (frameCount < learningPeriod)
            this.randomTeleport();
         else {
@@ -82,21 +81,21 @@ class RedDot {
     }
 
     randomTeleport () {
-        this.position.x = rand(0, this.island.size);
-        this.position.y = rand(0, this.island.size);
+        this.position.x = rand(0, this.room.size);
+        this.position.y = rand(0, this.room.size);
     }
 }
 
 //only cats for now
 class Creature {
-    constructor(island, vector, learningRate = 0.25){
-        this.island = island;
+    constructor(room, vector, learningRate = 0.25){
+        this.room = room;
         this.catSprite = PIXI.Sprite.from('/assets/images/Cat.png');
         this.catSprite.x = vector.x;
         this.catSprite.y = vector.y;
         this.position = vector.getCopy();
-        this.velocity = new Vector2d(0, 0, -5, 5);
-        this.accel = new Vector2d(0, 0, -0.5, 0.5);
+        this.velocity = new Vector2d(0, 0, -10, 10);
+        this.accel = new Vector2d(0, 0, -1, 1);
         this.learningRate = learningRate;
         this.fitness = 0;
         app.stage.addChild(this.catSprite);
@@ -109,26 +108,30 @@ class Creature {
     update() {
         //console.log(this.network);
 
-        //get this island's size and its red dot
-        var islandSize = this.island.size;
-        var redDot = this.island.redDot; 
+        //get this room's size and its red dot
+        var roomSize = this.room.size;
+        var redDot = this.room.redDot; 
         
-        //normalize positions by island size (for NN inputs)
-        var input = [(redDot.position.x / islandSize), (redDot.position.y / islandSize)];
-                   //(this.position.x / islandSize), (redDot.position.y / islandSize)];
+        //normalize positions by room size (for NN inputs)
+        var input = [(redDot.position.x / roomSize), (redDot.position.y / roomSize)];
+                   //(this.position.x / roomSize), (redDot.position.y / roomSize)];
         var output = this.network.activate(input);
 
         //the vector to be changed by output, such as velocity or accel
-        var changing = this.accel; 
+        var changing = this.velocity; 
+
         //update creature's acceleration from output
-        changing.x = (output[0] * 2 * changing.max) - changing.max;
-        changing.y = (output[1] * 2 * changing.max) - changing.max 
+        changing.x = output[0] > 0.5 ? output[0] : output[0] - 1;
+        changing.y = output[1] > 0.5 ? output[1] : output[1] - 1;
+        //console.log("Change");
+        //console.log(changing);
 
         // find intended output so NN can learn
-        // arrange position data for NN, for example: (-1 to 0) -> (0 -> 2) -> (0 -> 1)
-        var intendedX = (((redDot.position.x - this.position.x) / islandSize) + 1) / 2;
-        var intendedY = (((redDot.position.y - this.position.y) / islandSize) + 1) / 2;
+        var intendedX = this.position.x > redDot.position.x ? 0 : 1;
+        var intendedY = this.position.y > redDot.position.y ? 0 : 1;
         var intendedOutput = [intendedX, intendedY];
+        //console.log("intendedOutput");
+        //console.log(intendedOutput);
 
         //train neural network
         if (frameCount < learningPeriod)
@@ -140,30 +143,30 @@ class Creature {
         this.position.add(this.velocity);
 
         //out of bounds check
-        if (this.position.x > this.island.maxX-20){
+        if (this.position.x > this.room.size-20){
             //this.velocity.reverse();
             this.velocity.x = -this.velocity.x * 0.8;
             this.position.x -= 10;
         }
-        if (this.position.y > this.island.maxY-20){
+        if (this.position.y > this.room.size-20){
             //this.velocity.reverse();
             this.velocity.y = -this.velocity.y * 0.8;
             this.position.y -= 10;
         }
-        if (this.position.x < this.island.minX+20){
+        if (this.position.x < this.room.size+20){
             //this.velocity.reverse();
             this.velocity.x  = -this.velocity.x * 0.8;
             this.position.x += 10;
         }
-        if (this.position.y < this.island.minY+20){
+        if (this.position.y < this.room.size+20){
             //this.velocity.reverse();
             this.velocity.y = -this.velocity.y * 0.8;
             this.position.y += 10;
         }
 
         //set sprite x & y to position
-        this.catSprite.x = this.position.x;
-        this.catSprite.y = this.position.y;
+        this.catSprite.x = this.position.x + this.minX;
+        this.catSprite.y = this.position.y + this.minY;
 
         //reward
         //var distance = this.position.distance(redDot.position);
@@ -189,7 +192,16 @@ class Creature {
 
 //isolated 'ecosystems'
 class Island {
-    constructor(name, minX, minY, size, pop){
+    constructor(name, floor, minX, minY, size, pop){
+
+        //floor image
+        this.floor = PIXI.Sprite.from(floor);
+        this.floor.width = size;
+        this.floor.height = size;
+        this.floor.x = minX;
+        this.floor.y = minY;
+        app.stage.addChild(this.floor);
+
         this.minX = minX;
         this.minY = minY;
         this.maxX = minX + size;
@@ -199,8 +211,8 @@ class Island {
         this.pop = pop; //population
         this.creatures = new Array(pop);
         for (var c = 0; c < pop; c++)
-            this.creatures[c] = new Creature(this, new Vector2d(rand(minX,size), rand(minY,size))); //random positions
-            //((minX+this.maxX)/2, (minY+this.maxY)/2)); //start in middle of island
+            this.creatures[c] = new Creature(this, new Vector2d((minX+this.maxX)/2, (minY+this.maxY)/2)); //random positions
+            //((minX+this.maxX)/2, (minY+this.maxY)/2)); //start in middle of room
 
         this.redDot = new RedDot(this, new Vector2d((minX+this.maxX)/2, (minY+this.maxY)/2));
     }
@@ -234,12 +246,12 @@ class Island {
 
 }
 
-//cat converges on the red dot in the middle of the island
+//cat converges on the red dot in the middle of the room
 function worldLoop(delta){
     if (frameCount % fpsReduction === 0) {
         graphics.clear();
         for (var i=0; i < numIslands; i++){
-            islands[i].update();
+            rooms[i].update();
         }
     }
     frameCount++;
@@ -257,9 +269,17 @@ function rand(min, max) {
 }
 
 function start() {
-    //create islands
-    for (var i=0; i < numIslands; i++)
-        islands.push(new Island(islandNames[i], 50 * i * 2.5, 0, 500, initialPop));
+    //create rooms
+    for (var i=0, col=0, row=0; i < numIslands; i++) {
+        var originX = roomSize * col * 1.5;
+        var originY = roomSize * row * 1.5;
+        rooms.push(new Island(roomNames[i], roomFloors[i % 2], originX, originY, roomSize, initialPop));
+        col++;
+        if (originX > 500) {
+            col = 0;
+            row++;
+        }
+    }
     //send worldLoop to Pixi ticker
     app.ticker.add(delta => worldLoop(delta));
     app.stage.addChild(graphics);
